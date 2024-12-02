0195bcbfaa1d658c86fe32c7d149885888845c70
Jason <jaseagle2003@gmail.com>
2024-12-02 14:25:06 -0500

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      data: null
+    }
+  }
+
+  readCsv = () => {
+    var csvdata = []
+    
+
+    this.setState({data: csvdata})
+  }
+
+  componentDidMount() {
+    this.readCsv()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{marginLeft: "20px"}}>CS 450 Project Dashboard</h1>
+        <div className="visualizations">
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
